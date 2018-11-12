3def82901daf0de2cab92bd79f26759ce66b1bd8
yangzhixin <yangzhixin@hisense.com>
2018-11-12 18:23:23 +0800

    完善插入排序,初始化冒泡排序

--- Md-sort.js
+++ Md-sort.js
@@ -39,15 +39,47 @@ function insertionSort(arr) {
   for(var i=1; i< arr.length; i++) {
 
     var temp = arr[i];
-    var j = i-1;
-    console.log(j, '当前位置temp:', temp, arr);
-    while (j>=0 && temp < arr[j]) {   // 向左比较，若当前对象比前一个小，则开始比对。
-      arr[j+1] = arr[j];   // 把当前位置跟左侧对象 替换
-      j = j-1;
-      console.log('    然后j-1, j=', j, arr);
+    var j = i;
+    //console.log(j, '当前位置temp:', temp, arr);
+    while (j>=0 && temp < arr[j-1]) {   // 向左比较，若当前对象比前一个小，则开始比对。
+      arr[j] = arr[j-1];   // 把当前位置跟左侧对象 替换
+      j--;
+      //console.log('    然后j-1, j=', j, arr);
     }
-    arr[j+1] = temp;
-    console.log(j, '位置改成', temp, arr);
+    arr[j] = temp;
+    //console.log(j, '位置改成', temp, arr);
   }
   return arr;
 }
+
+// 测试插入排序
+console.log(insertionSort(arrA));
+
+
+/**
+ * 冒泡排序
+ * 元素依次像气泡似的逐渐像后排，最大的在最后边（前边）。
+ * 
+ * 两个嵌套的循环. 其中, 外层循环移动游标; 内层循环遍历游标及之后(或之前)的元素, 
+ * 通过两两交换的方式, 每次只确保该内循环结束位置排序正确, 然后内层循环周期结束, 
+ * 交由外层循环往后(或前)移动游标, 随即开始下一轮内层循环, 以此类推, 直至循环结束.
+ * 
+ * 平均时间复杂度 O(n²) ，最好 O(n)， 最坏 O(n²)
+ * 空间复杂度 O(1)
+ */
+function bubbleSort(arr) {
+  for (var i=0;i<arr.length;i++) {
+    var temp = arr[i];
+    console.log(temp);
+
+    for (var j=i+1;j<arr.length;j++) {
+      if (temp > arr[j]){
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+// 测试冒泡排序
+console.log(bubbleSort(arrA));
